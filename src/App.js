778451a86575fd32f877387da4151b689355ac03
livetowordlife <livetoworldlife@gmail.com>
2020-07-09 00:23:14 +0200

    starting a project  Hack Your Weather!

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CityData from './cards';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Weather </h1>
+      <div>
+        <form></form>
+      </div>
+      <CityData />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/cards.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import CityWeather from './city-weather.json';
+
+
+
+export default function GetCityData() {
+  const newData = CityWeather.map(data => {
+    const {
+      id,
+      name,
+      sys: { country },
+      weather: [{ main, description }],
+      main: { temp_min, temp_max },
+      coord: { lat, lon } } = data;
+
+    return (
+      <div key={id}>
+        <div className='card'>
+          <div className="close" ></div>
+          <h4>
+            <a href={id}>
+              {name},{country}
+            </a>
+          </h4>
+          <h6>{main}</h6>
+          <p>{description}</p>
+          <p>min temp: {temp_min}</p>
+          <p>max temp: {temp_max}</p>
+          <p>location: {lat}, {lon}</p>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div>
+      {newData}
+    </div>
+  );
+}
